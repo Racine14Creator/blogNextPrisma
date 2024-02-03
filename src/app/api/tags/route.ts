8f941308648f9df62eed08acf7f27c713eada665
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-02-03 07:34:02 +0200

    Api and Get event

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
     try {
         const tags = await db.tag.findMany()
+        // console.log(tags)
         return NextResponse.json(tags, { status: 200 })
     } catch (error) {
         return NextResponse.json({ message: "Could not fetch tags" }, { status: 500 })
