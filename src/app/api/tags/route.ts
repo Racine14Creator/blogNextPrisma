9178be7725983a59f2d4f2fcc438263481af3e2c
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-02-06 14:41:49 +0200

    Ready data

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
     try {
         const tags = await db.tag.findMany()
-        // console.log(tags)
+        // console.log(tags) test data
         return NextResponse.json(tags, { status: 200 })
     } catch (error) {
         return NextResponse.json({ message: "Could not fetch tags" }, { status: 500 })
